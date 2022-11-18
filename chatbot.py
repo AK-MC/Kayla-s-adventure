8a986e91d9bf8bf8c8e11e57219b8dd0a38866f2
Monisse Cunha <monisse.dc@aluno.ifsc.edu.br>
2022-11-18 20:04:24 +0000

    Modificar código

--- chatbot.py
+++ chatbot.py
@@ -40,8 +40,7 @@ async def on_message(msg):
         #
         # Jogador começa no estado 0 com duas chaves
         partidas[autor] = {
-            'estado': 0,
-            'inventario': set()
+            'estado': 0
         }
     #
     # Testar se o canal é pvt (msg.channel.type.name == 'private')
@@ -67,43 +66,31 @@ async def on_message(msg):
     #
     # Criar variáveis locais para melhorar legibilidade do código
     estado_do_jogador = estados[partidas[autor]['estado']]
-    inventario_do_jogador = partidas[autor]['inventario']
     #
     # Varrer os possíveis próximos estados para validar com a mensagem do usuário
     for key, value in estado_do_jogador['proximos_estados'].items():
         if fullmatch(key, mensagem):
             #
-            # Verificar se o jogador possui inventário mínimo para avançar
-            if inventario_do_jogador.issuperset(estados[value]['inventario']):
-                #
-                # Atualiza o estado do jogador
-                partidas[autor]['estado'] = value
-                #
-                # Remove os itens de inventário requisitados
-                partidas[autor]['inventario'] = inventario_do_jogador.difference(
-                    estados[value]['inventario'])
-                #
-                # Se houver um som referente ao estado,
-                # toca no canal de voz do jogador
-                if msg.channel.type.name != 'private':
-                    arquivo_de_som = str(value) + '.mp3'
-                    if exists(arquivo_de_som):
-                        #
-                        # Conectar no canal de áudio e emitir o som
-                        som_opus = await discord.FFmpegOpusAudio.from_probe(arquivo_de_som)
-                        canal_de_voz.play(som_opus)
-                #
-                # Se houver uma imagem referente ao estado, enviar
-                arquivo_de_imagem = str(value) + '.png'
-                if exists(arquivo_de_imagem):
-                    await msg.channel.send(file=discord.File(arquivo_de_imagem))
-                #
-                # Criar uma lista de frases usando o delimitador '|' e enviar uma a uma
-                [await msg.channel.send(i) for i in choice(estados[value]['frases']).split('|')]
-            else:
-                #
-                # Retornar mensagem (e manter jogador no atual estado)
-                await msg.channel.send(frases['inventario_insuficiente'])
+            # Atualiza o estado do jogador
+            partidas[autor]['estado'] = value
+            #
+            # Se houver um som referente ao estado,
+            # toca no canal de voz do jogador
+            if msg.channel.type.name != 'private':
+                arquivo_de_som = str(value) + '.mp3'
+                if exists(arquivo_de_som):
+                    #
+                    # Conectar no canal de áudio e emitir o som
+                    som_opus = await discord.FFmpegOpusAudio.from_probe(arquivo_de_som)
+                    canal_de_voz.play(som_opus)
+            #
+            # Se houver uma imagem referente ao estado, enviar
+            arquivo_de_imagem = str(value) + '.png'
+            if exists(arquivo_de_imagem):
+                await msg.channel.send(file=discord.File(arquivo_de_imagem))
+            #
+            # Criar uma lista de frases usando o delimitador '|' e enviar uma a uma
+            [await msg.channel.send(i) for i in choice(estados[value]['frases']).split('|')]
             return
     #
     # Sempre responder ao usuário (dica ou não)
@@ -112,4 +99,4 @@ async def on_message(msg):
     else:
         await msg.channel.send(frases['erro'])
 
-bot.run(getenv('DISCORD_TOKEN'))
+bot.run(getent('DISCORD_TOKEN'))
